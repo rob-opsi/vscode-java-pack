4f7659316415967a4db6ee028db63786941cc8f5
Rome Li <roml@microsoft.com>
2018-05-29 18:24:03 +0800

    add bootstrap support

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,22 @@ module.exports = function (env, argv) {
       rules: [{
         test: /\.ts$/,
         use: 'ts-loader'
+      }, {
+        test: /\.(scss)$/,
+        use: [{
+          loader: 'style-loader'
+        }, {
+          loader: 'css-loader'
+        }, {
+          loader: 'postcss-loader',
+          options: {
+            plugins: function () {
+              return [require('autoprefixer')];
+            }
+          }
+        }, {
+          loader: 'sass-loader'
+        }]
       }]
     },
     mode: 'development',
