75b524d36b550a4514ab012d2765caa2f31fbb74
Rome Li <rome.lee@gmail.com>
2018-09-10 13:02:55 +0800

    Instrument telemetry

--- src/extension.ts
+++ src/extension.ts
@@ -1,18 +1,35 @@
 import * as vscode from 'vscode';
+import { initialize, instrumentOperation } from 'vscode-extension-telemetry-wrapper';
 
-import { instrumentCommand } from "./command";
+import { instrumentCommand } from './command';
 import { overviewCmdHandler, createMavenProjectCmdHanlder, createSpringBootProjectCmdHandler, showExtensionCmdHandler, showOverviewPageOnActivation } from './overview';
 
-export function activate(context: vscode.ExtensionContext) {
-  context.subscriptions.push(vscode.commands.registerCommand('java.overview', instrumentCommand(context, overviewCmdHandler)));
+export async function activate(context: vscode.ExtensionContext) {
+  initializeTelemetry(context);
 
-  context.subscriptions.push(vscode.commands.registerCommand('java.helper.createMavenProject', instrumentCommand(context, createMavenProjectCmdHanlder)));
+  const activationOperation = instrumentOperation('activation', initializeExtension);
+  await activationOperation(context);
+}
 
-  context.subscriptions.push(vscode.commands.registerCommand('java.helper.createSpringBootProject', instrumentCommand(context, createSpringBootProjectCmdHandler)));
+async function initializeExtension(operationId: string, context: vscode.ExtensionContext) {
+  context.subscriptions.push(vscode.commands.registerCommand('java.overview', instrumentCommand(context, 'java.overview', overviewCmdHandler)));
 
-  context.subscriptions.push(vscode.commands.registerCommand('java.helper.showExtension', instrumentCommand(context, showExtensionCmdHandler)));
+  context.subscriptions.push(vscode.commands.registerCommand('java.helper.createMavenProject', instrumentCommand(context, 'java.helper.createMavenProject', createMavenProjectCmdHanlder)));
 
-  showOverviewPageOnActivation(context);
+  context.subscriptions.push(vscode.commands.registerCommand('java.helper.createSpringBootProject', instrumentCommand(context, 'java.helper.createSpringBootProject', createSpringBootProjectCmdHandler)));
+
+  context.subscriptions.push(vscode.commands.registerCommand('java.helper.showExtension', instrumentCommand(context, 'java.helper.showExtension', showExtensionCmdHandler)));
+
+  await showOverviewPageOnActivation(context);
+}
+
+function initializeTelemetry(context: vscode.ExtensionContext) {
+  const packageInfo = require(context.asAbsolutePath('./package.json'));
+  if (packageInfo) {
+    if (packageInfo.aiKey) {
+      initialize(packageInfo.name, packageInfo.version, packageInfo.aiKey, true);
+    }
+  }
 }
 
 export function deactivate() {
